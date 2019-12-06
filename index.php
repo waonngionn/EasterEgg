fec0056f5355bcfce288ab904bececfbd99cfab4
mitonaoki <air_mito.0622@icloud.com>
2019-12-06 17:14:23 +0900

    GoogleAnalytics導入

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154162030-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-154162030-1');
+        </script>
+        <!-- /Global site tag (gtag.js) - Google Analytics -->
+
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <meta charset="utf-8">
         <title>EasterEgg</title>
